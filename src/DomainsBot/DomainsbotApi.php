dddc016a69d851f610cb2f10fc546014dd8a0343
Giulio Prina Ricotti <giulio.prinaricotti@araundu.it>
2015-12-31 16:00:38 +0100

    API Client: updated "auth-token" to "apikey" API Client: changed base URL API Client: moved all to src/DomainsBot Readme: updated documentation [NEW] composer.json

--- src/DomainsBot/DomainsbotApi.php
+++ src/DomainsBot/DomainsbotApi.php
@@ -1,64 +1,64 @@
 <?php
-ini_set('display_errors', 1); 
-error_reporting(E_ALL);
+
+namespace DomainsBot;
 
 class DomainsbotApi
 {
-	
+
 	public function __construct($token)
 	{
-		$this->urlToPost = 'http://api.domainsbot.com/v4/spinner?';
+		$this->urlToPost = 'https://api-2445581410012.staging.apicast.io/v5/recommend?';
 		$this->token = $token;
 	}
-    
+
     public function GetSuggestion ($key, array $params = array(), $getJSON = true)
     {
-	
+
 	// Post string to DomainBot api, Build the request string from global variables
 	$requestString = $this->urlToPost . "q=".$key;
-	
+
 	foreach ($params as $k => $value) {
 	    $requestString .= "&". $k . "=" . $value;
 	}
-	
-	$requestString .= "&auth-token=" . $this->token;
-	
+
+	$requestString .= "&apikey=" . $this->token;
+
 	if( $getJSON)
 		return json_decode($this->SendRequest($requestString));
 	else
 		return $this->SendRequest($requestString);
-		
+
     }
-    
+
     public function GetSuggestionRawUrl ($rawUrl, $getJSON = true)
     {
-	$requestString = $this->urlToPost .$rawUrl . "&auth-token=" . $this->token;
-	
+	$requestString = $this->urlToPost .$rawUrl . "&apikey=" . $this->token;
+
 	if( $getJSON)
 		return json_decode($this->SendRequest($requestString));
 	else
 		return $this->SendRequest($requestString);
-    }  
-    
+    }
+
     private function SendRequest ($url)
     {
 		try // Try Block
 		{
-			
-						
+
+
 			$opts = array(
 			  'http'=>array(
 			    'method'=>"GET"
 			  )
 			);
-			
-			$context = stream_context_create($opts);   
+
+			$context = stream_context_create($opts);
 
 			$fp = fopen($url, 'r', false, $context);
 			$contents = stream_get_contents($fp);
 			fclose($fp);
-			
-			
+
+
 			// Print the final result
 			return $contents;
 		}
